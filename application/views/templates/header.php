cd0185afe4cca2b42f5231a917bb2334a3885d4f
William Boone <138145899+boonewh@users.noreply.github.com>
2023-12-21 21:48:37 -0600

    mobile styling finished, tablet styling done

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,8 +5,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="Center for Social Success"/>
 		<link rel="preconnect" href="https://fonts.googleapis.com">
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                <link href="https://fonts.googleapis.com/css2?family=Oleo+Script&display=swap" rel="stylesheet">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Oleo+Script&display=swap" rel="stylesheet">
 		<title><? echo $title; ?></title> <!--Center for Social Success || Dallas, Tx.-->
 		<link rel="stylesheet" href="assets/css/main.css">
 		<link rel="icon" href="assets/images/favicon.png" type="image/png">
@@ -16,12 +16,12 @@
 		<header>
 			<div id="toptop">
 				<p id="topOne">Social, Emotional, and Behavioral Skills</p>
-				<p id="topTow">Call Us: 972-404-3001</p>
+				<p id="topTwo">Call Us: 972-404-3001</p>
 			</div>
 			
 			<nav class="navFirst">
-				<img id="mainLogo" src="assets/images/css-logo4.png" alt="Logo">
 				<a href="#" id="navicon"><img src="assets/images/navicon.png" alt=""></a>
+				<img id="mainLogo" src="assets/images/csslogo-white.png" alt="Logo">
 				<ul>
 					<li><a href="index.html">Home Page</a></li>
 					<li><a href="html/services.html">Services</a></li>
