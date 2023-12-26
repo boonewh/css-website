48aaa1f6658be4d3e269417f0fa44d067bd9ae41
William Boone <138145899+boonewh@users.noreply.github.com>
2023-12-25 22:52:26 -0600

    Adding individual page files to views/pages

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,18 +1,40 @@
 <?php
 class Pages extends CI_Controller {
 
+        private $homeData = [
+                'title' => "Center for Social Success || Dallas, Tx.",
+                'description' => "Let us help you in all of your social interactions. Family, individual and group therapy including animal assisted services",
+                'css' => "assets/css/main.css"
+        ];
+
+        private $servicesData = [
+                'title' => "CSS Services Page",
+                'description' => "The services that CSS provide. Adult Services; Child, Teen, Family Services; Animal Assisted Services; Social Skills Groups.",
+                'css' => "assets/css/main.css",
+        ];
+
         public function view($page = 'home')
         {
-            if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
-            {
-                    // Whoops, we don't have a page for that!
-                    show_404();
-            }
-    
-            $data['title'] = ucfirst($page); // Capitalize the first letter
-    
-            $this->load->view('templates/header', $data);
-            $this->load->view('pages/'.$page, $data);
-            $this->load->view('templates/footer', $data);
+
+                if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
+                {
+                        // Whoops, we don't have a page for that!
+                        show_404();
+                }
+
+                    // Load the correct template data.
+                if ($page === 'home') {
+                $data = $this->homeData;
+                } else if ($page === 'services') {
+                        $data = $this->servicesData;
+                } else {
+                        show_404();
+                }
+        
+                $data['title'] = ucfirst($page); // Capitalize the first letter
+        
+                $this->load->view('templates/header', $data);
+                $this->load->view('pages/'.$page, $data);
+                $this->load->view('templates/footer', $data);
         }
 }
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,12 +3,12 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="Let us help you in all of your social interactions. Family, individual and group therapy including animal assisted services."/>
+		<meta name="description" content=<?=$description; ?>/> <!--Let us help you in all of your social interactions. Family, individual and group therapy including animal assisted services.-->
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<link href="https://fonts.googleapis.com/css2?family=Oleo+Script&display=swap" rel="stylesheet">
-		<title><? echo $title; ?></title> <!--Center for Social Success || Dallas, Tx.-->
-		<link rel="stylesheet" href="assets/css/main.css">
+		<title><?=$title; ?></title> <!--Center for Social Success || Dallas, Tx.-->
+		<link rel="stylesheet" href=<?=$css; ?>>   <!--"assets/css/main.css"-->
 		<link rel="icon" href="assets/images/favicon.png" type="image/png">
 	</head>
 
@@ -25,11 +25,13 @@
 				<nav class="navFirst">
 					<a href="#" id="navicon"><img src="assets/images/navicon.png" alt=""></a>
 					<ul>
-						<li><a href="index.html">Home Page</a></li>
-						<li><a href="html/services.html">Services</a></li>
-						<li><a href="html/staff.html">Our Staff</a></li>
-						<li><a href="html/location.html">Location</a></li>
-						<li><a href="html/contact.html">Contact Us</a></li>
+						<li><a href="/home">Home Page</a></li>
+						<li><a href="/services">Services</a></li>
+						<li><a href="/staff">Our Staff</a></li>
+						<li><a href="/pups">Therapy Pups</a></li>
+						<li><a href="/newsletter">Newsletter</a></li>
+						<li><a href="/location">Location</a></li>
+						<li><a href="/contact">Contact Us</a></li>
 					</ul>
 				</nav>
 			</div>
