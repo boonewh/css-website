b10a4e94180772c52a46a8f95eab5d77be55c734
William Boone <138145899+boonewh@users.noreply.github.com>
2023-12-27 22:17:30 -0600

    Staff page finished v1

--- application/views/pages/staff.php
+++ application/views/pages/staff.php
@@ -5,9 +5,8 @@
             <div class="container">
                 <div class="staff">
                     <img src="assets/images/erin.jpg" alt="Erin Lozano">
-                    <h4>Erin Lozano</h4>
+                    <h4>Erin Lozano, M.Ed, LPC-Supervisor, RPT-S</h4>
                     <h5>Early Childhood Division Director</h5>
-                    <h5>M.Ed, LPC-Supervisor, RPT-S</h5>
                     <ul>
                         <li>Specializations Include:</li>
                         <li>Play Therapy</li>
@@ -16,7 +15,7 @@
                         <li>Anxiety Management</li>
                         <li>Anger Management</li>
                         <li>Sibling Therapy</li>
-                        <li>PCIT (Parent-Child Interaction Therapy)</li>
+                        <li>PCIT</li>
                     </ul>
                 </div>
                 <div class="staff">
@@ -36,29 +35,39 @@
                 </div>
             </div>
             <div class="container">
-                <div class="staff">
-                    <img src="assets/images/tobi.jpg" alt="Tobi Carter">
-                    <h4>Tobi Carter, LMSW</h4>
-                    <h5>Chief Medical Officer</h5>
-                </div>
-                <div class="staff">
-                    <img src="assets/images/deliah.jpg" alt="Deliah Mooney">
-                    <h4>Deliah Mooney, LMSW</h4>
-                    <h5>Chief Wellness Coach</h5>
-                </div>
-                <div class="staff">
-                    <img src="assets/images/roy.jpg" alt="Roy Rodriguez">
-                    <h4>Roy Rodriguez</h4>
-                    <h5>Chief Welness Coordinator</h5>
-                </div>
-            </div>
-            <div class="staff">
-                <img src="assets/images/dristre" alt="Dr. Susan Istre">
+            <div class="dristre">
+                <img src="assets/images/dristre.png" alt="Dr. Susan Istre">
                 <h4>Susan Istre, PhD, LPC-S</h4>
                 <h5>Director Ameritas</h5>
             </div>
-            <div class="staff">
-                <img src="assets/images/melissa" alt="Melissa Drager">
+                <div class="staff2">
+                    <img src="assets/images/tobi.jpg" alt="Tobi Carter">
+                    <div>
+                        <h4>Tobi Carter, MSW, LMSW</h4>
+                        <h5>Chief Medical Officer</h5>
+                    </div>
+                </div>
+                <div class="staff2">
+                    <div>
+                        <h4>Deliah Mooney, MSW, LMSW</h4>
+                        <h5>Chief Wellness Coach</h5>
+                    </div>
+                    <img src="assets/images/deliah.jpg" alt="Deliah Mooney">
+                </div>
+                <div class="staff2">
+                    <img src="assets/images/roy.jpg" alt="Roy Rodriguez">
+                    <div>
+                        <h4>Roy Rodriguez, MSW, LMSW</h4>
+                        <h5>Chief Welness Coordinator</h5>
+                    </div>
+                </div>
+                <div class="staff2">
+                    <h4>Mattie Crownover, MSW, LMSW</h4>
+                    <img src="assets/images/mattie.jpg" alt="Mattie Crownover">
+                </div>
+            </div>
+            <div class="mel">
+                <img src="assets/images/melissa.jpg" alt="Melissa Drager">
                 <h4>Melissa Drager</h4>
                 <h5>Practice Manager</h5>
             </div>
