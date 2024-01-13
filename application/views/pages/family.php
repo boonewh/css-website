80a9c9ec53daa826fe7847dc394df2b51a6420ac
William Boone <138145899+boonewh@users.noreply.github.com>
2024-01-12 20:52:21 -0600

    creating individual therapy pages per client

---
+++ application/views/pages/family.php
@@ -0,0 +1,26 @@
+        <main>
+                
+            <hr>
+            <div class="therapy-container">
+                <a href="/social"><h2>Social Skills Group</h2></a></a>
+                <a href="/play"><h2>Play Therapy</h2></a></a>
+                <a href="/teen"><h2>Teen and Adolescents Therapy</h2></a></a>
+                <a href="/animal"><h2>Animal Assisted Therapy</h2></a></a>
+                <a href="/adult"><h2>Young Adult and Adult Therapy</h2></a></a>
+                <a href="/family"><h2>Family/Parenting Therapy</h2></a></a>
+            </div>
+            <hr>
+            <h1 id="family">Family/Parenting Therapy</h1>
+            <br>
+            <br>
+            <div class= therapy-container2>
+                <section class="therapy">
+                    <img src="assets/images/familyservices.jpg" alt="Child, Teen, Family" class="center-logo">
+                    <div>
+                        <p class="first-paragraph">
+
+                        </p>
+                    </div>
+                </section>
+            </div>            
+        </main>
