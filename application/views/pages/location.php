5706e12736b5dde164447cd18b0dba214a092154
William Boone <138145899+boonewh@users.noreply.github.com>
2024-01-21 13:54:38 -0600

    modified location file

--- application/views/pages/location.php
+++ application/views/pages/location.php
@@ -8,7 +8,7 @@
                 <div>
                     <p>Main Office:</p>
                     <p>12880 Hillcrest Rd</p>
-                    <p>Ste 105</p>
+                    <p>Ste J105</p>
                     <p>Dallas, Tx 75230</p>
                 </div>
             </div>   
